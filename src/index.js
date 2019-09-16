4dc731c97db5f7cf779c0f4c0e6ac7e438ed1f69
skipkayhil <skipkayhil@gmail.com>
2019-09-16 15:38:18 -0400

    pre gatsby

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import { h, app } from '../lib/hyperapp.min.js';
+import { h, app } from "../lib/hyperapp.min.js";
 
 console.log("test");
 
-const view = () => (
-    h("div", { class: 'header' }, [
-      h("h1", {}, "Hartley McGuire"),
-      h("p", {}, "Computer Science Major | Georgia Institute of Technology"),
-      h("button", {}, "Projects")
-    ])
-);
+const view = () =>
+  h("div", { class: "header" }, [
+    h("h1", {}, "Hartley McGuire"),
+    h("p", {}, "Computer Science Major | Georgia Institute of Technology"),
+    h("button", {}, "Projects")
+  ]);
 
 app({}, {}, view, document.body);
